cd3d347f5489186b843e253980eb6cb7df1c0d6d
splendidcomputer <splendidcomputer@gmail.com>
2023-10-20 22:10:23 +0200

    Transformed array of objects  into a two-dimensional data matrix.

---
+++ apps/cli/README.md
@@ -0,0 +1,23 @@
+Use the following command to install the `ml-hclust` package:
+
+```bash
+pnpm install ml-hclust -w
+```
+
+Install pandas-js:
+
+```bash
+pnpm install pandas-js
+```
+
+We should do the following in the root of the project:
+
+```bash
+pnpm install
+```
+
+then we should run the following in the `apps/cli` directory where the package.json file is located:
+
+```bash
+pnpm run hi
+```
--- apps/cli/src/main.ts
+++ apps/cli/src/main.ts
@@ -1,29 +1,28 @@
-import { promises } from 'node:fs';
-import path from 'node:path';
+import { promises } from "node:fs";
+import path from "node:path";
+// import { create, all } from "mathjs";
 
-const __dirname = path.dirname(import.meta.url)
-const fileUrl = new URL(`${__dirname}/../data/data.csv`)
-console.log('Reading the data.csv...', fileUrl)
+const __dirname = path.dirname(import.meta.url);
+const fileUrl = new URL(`${__dirname}/../data/data.csv`);
+console.log("Reading the data.csv...", fileUrl);
 try {
-  const data = await promises.readFile(fileUrl , 'utf8')
-  let i=0
-  const lines = data.split(/\r?\n/)
-  const fieldNames = lines[0].split(';').slice(0,20)
-  const result = []
-  for(let i=1;i<lines.length;i++) {
-    const fieldValues = lines[i].split(';')
-    const o: Record<string, number> = { line: i}
-    for(let j=0;j<fieldNames.length;j++) {
-      o[fieldNames[j]] = parseInt(fieldValues[j])
+  const data = await promises.readFile(fileUrl, "utf8");
+  let i = 0;
+  const lines = data.split(/\r?\n/);
+  const fieldNames = lines[0].split(";").slice(0, 52);
+  const result = [];
+  for (let i = 1; i < lines.length; i++) {
+    const fieldValues = lines[i].split(";");
+    const o: Record<string, number> = { line: i };
+    for (let j = 0; j < fieldNames.length; j++) {
+      o[fieldNames[j]] = parseInt(fieldValues[j]);
     }
-    result.push(o)
+    result.push(o);
   }
-//  console.log(fieldNames)
- console.table(result)
-} catch(err) {
-  console.error(err)
+  //  console.log(fieldNames)
+  const dataMat = result.map((obj) => Object.values(obj));
+
+  console.log(dataMat);
+} catch (err) {
+  console.error(err);
 }
-
-  
-  
-
